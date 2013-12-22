b3b5a5ce1293c3c4049d0e07b0ec962bca465b7d
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2013-12-22 14:01:42 +0000

    added new single view project

---
+++ HowToDoWudu/HowToDoWudu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HowToDoWudu
+//
+//  Created by Dilwoar Hussain on 22/12/2013.
+//  Copyright (c) 2013 CosmicInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
